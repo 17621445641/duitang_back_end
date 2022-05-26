4e6db8192a6ce451f23585f4526564f535b159f6
kxp <2480274726@qq.com>
2022-05-26 18:37:49 +0800

    提交看下

---
+++ flaskr/user_message2.py
@@ -0,0 +1,13 @@
+from flask import Flask,request
+import security
+from flaskr import db
+import json
+def ww(server):
+# server=Flask(__name__)
+    @server.route('/userinfo',methods=['get'])
+    def userinfo():
+        token = request.headers['access_token']  # 获取header里的token
+        parse_token=security.parse_token(token)
+        return security.parse_token(parse_token)
+    # if __name__ == '__main__':
+    #     server.run(host='127.0.0.1',port=8998,debug=True)
---
+++ test/user_message_post接口.py
@@ -0,0 +1,23 @@
+from flask import Flask,request
+from flaskr import db
+import json
+server=Flask(__name__)
+sql='SELECT * from user_message '
+@server.route('/login',methods=['post'])
+def login():
+    user_name=request.json.get('account')
+    sex = request.json.get('password')
+    # token=request.headers['access_token']#获取header里的token
+    if(user_name=="测试用户" and sex=='男'):
+        res=db.my_db(sql)
+        tinydict = {'Name': '', 'Age': '', 'sex': ''}
+        tinydict['Name']=res[0][1]
+        tinydict['Age'] = res[0][3].strftime('%Y-%m-%d')
+        tinydict['sex'] = res[0][2]
+        return json.dumps(tinydict, ensure_ascii=False, indent=4)
+    else:
+        message="用户有误"
+        # return json.dumps(message, ensure_ascii=False, indent=4)
+        return message
+server.run(host='127.0.0.1',port=8998,debug=True)
+
